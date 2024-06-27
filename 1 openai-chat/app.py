1fd27ed3aa91aabd8d09ac938e96da186d83f7be
VRamirezMoodys <Victor.Ramirez@moodys.com>
2024-06-27 15:17:50 -0700

    updated demo 1 project

--- 1 openai-chat/app.py
+++ 1 openai-chat/app.py
@@ -10,6 +10,7 @@ load_dotenv()
 # Get your OpenAI API key
 openai_api_key="your-api-key"
 
+
 # Initialize OpenAI client with your API key
 client = AsyncOpenAI(api_key=openai_api_key)
 
@@ -49,20 +50,28 @@ async def on_message(message: cl.Message):
     # Call the tool
     tool()
 
-    # TODO: Generate a response using the OpenAI model
+    # Generate a response using the OpenAI model
+
+    # This line starts an asynchronous operation using 'await'. It means the code waits for the operation to complete before moving on.
     response = await client.chat.completions.create(
+        # 'messages' is a list of dictionaries, each representing a message.
         messages=[
-            ########################################################
-            # TODO add missing code here
-
-
-
-
-            ########################################################
+            {
+                # The first message is from the system, setting the context that the bot replies in Spanish.
+                "content": "You are a helpful bot, you always reply in Spanish",
+                "role": "system"
+            },
+            {
+                # The second message is from the user. 'message.content' contains the user's message.
+                "content": message.content,
+                "role": "user"
+            }
         ],
+        # '**settings' unpacks additional settings for the 'create' method, which could include language, temperature, etc.
+        # using **settings in the function call is equivalent to directly passing these as keyword arguments like:
+        # model="text-davinci-003", temperature=0.7.
         **settings
     )
-
 
     # Send the response back to the user
     await cl.Message(content=response.choices[0].message.content).send()
